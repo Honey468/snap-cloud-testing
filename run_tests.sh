fa06797bd803753f82e3e2a0250866b6884df269
Michael Ball <cycomachead@gmail.com>
2018-09-22 02:34:13 -0700

    Allow passing busted command line args to run_tests

--- run_tests.sh
+++ run_tests.sh
@@ -32,7 +32,7 @@ psql -U cloud -d ${DATABASE_NAME} -a -f snapCloud/cloud.sql > /dev/null
 
 echo 'Running tests...'
 # Run the tests in the spec directory with resty nginx libraries
-cd snapCloud && resty -I ../spec/ ../resty_busted.lua ../spec
+cd snapCloud && resty -I ../spec/ ../resty_busted.lua ../spec $@
 results=$?
 
 # Cleanup postgres.
